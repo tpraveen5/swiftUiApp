f155bace0484492425f2b6add6728946ebfd9c7a
praveen <praveen.talari@touchalife.org>
2024-10-29 10:45:08 +0530

    Initial Commit

---
+++ BaseSwiftUI/BaseSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaseSwiftUIApp.swift
+//  BaseSwiftUI
+//
+//  Created by Praveen Talari on 29/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BaseSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BaseSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BaseSwiftUI
+//
+//  Created by Praveen Talari on 29/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
