28a3d164f844819644742c30ed74ad31f4b75f49
praveen <praveen.talari@touchalife.org>
2024-12-04 10:41:15 +0530

    Updated 04 Dec

---
+++ BaseSwiftUI/NewAnimatedView.swift
@@ -0,0 +1,18 @@
+//
+//  NewAnimatedView.swift
+//  BaseSwiftUI
+//
+//  Created by Praveen Talari on 13/11/24.
+//
+
+import SwiftUI
+
+struct NewAnimatedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewAnimatedView()
+}
